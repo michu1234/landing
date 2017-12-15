76f274c173f4ded8c65cb00977a7deabd1229a1f
michu1234 <moooderator@gmail.com>
2017-12-15 21:02:36 +0100

    more

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-let daysPlaceholder = document.querySelector(".inputForm__timer-item:first-child");
-let hourPlaceholder = document.querySelector(".inputForm__timer-item:nth-child(2)");
-let minutesPlaceholder = document.querySelector(".inputForm__timer-item:nth-child(3)");
-let secondsPlaceholder = document.querySelector(".inputForm__timer-item:nth-child(4)");
+window.onload = function (e) {
+
+    // SET DATE TO COUNTDOWND...
+    let eventTime = moment('24-12-2017 00:00:00', 'DD-MM-YYYY HH:mm:ss').unix(),
+        currentTime = moment().unix(),
+        diffTime = eventTime - currentTime,
+        duration = moment.duration(diffTime * 1000, 'milliseconds'),
+        interval = 1000;
+    daysPlaceholder = document.querySelector(".inputForm__timer-item:first-child");
+    hourPlaceholder = document.querySelector(".inputForm__timer-item:nth-of-type(2)");
+    minutesPlaceholder = document.querySelector(".inputForm__timer-item:nth-of-type(3)");
+    secondsPlaceholder = document.querySelector(".inputForm__timer-item:nth-of-type(4)");
 
 
+    if (diffTime > 0) {
+        setInterval(function () {
 
+            duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
+            var d = moment.duration(duration).days(),
+                h = moment.duration(duration).hours(),
+                m = moment.duration(duration).minutes(),
+                s = moment.duration(duration).seconds();
 
+            daysPlaceholder.innerHTML = d + "d";
+            hourPlaceholder.innerHTML = h + "h";
+            minutesPlaceholder.innerHTML = m + "m";
+            secondsPlaceholder.innerHTML = s + "s";
+        }, interval);
 
+    }
 
-var eventTime = moment([2017, 12, 24]);
-var currentTime = moment.format(s); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
-
-var diff = currentTime.from(eventTime)
-console.log(diff);
-
-
-var diffTime = eventTime - currentTime;
-var duration = moment.duration(diffTime*1000, 'milliseconds');
-var interval = 1000;
-
-setInterval(function(){
-  duration = moment.duration(duration - interval, 'milliseconds');
-    $('.countdown').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
-}, interval);
-
+};
